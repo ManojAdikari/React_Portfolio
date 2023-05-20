ee25a1f9e8288735d9c0845c243411d485c3d90e
Manoj Adikari <manojpadikari@gmail.com>
2023-05-20 18:14:06 +0930

    Fix classname err.

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -11,7 +11,7 @@ function Resume() {
                     <div className="info-book">
                         <h2 className="display-4 text-info" style={{ marginTop: "50px" }}>My Skills</h2>
                         <p className="lead mt-3">My programming language and tools </p>
-                        <a href="https://docs.google.com/document/d/1ZZvnRJOo-vOrAwvU0rlj1YT0z-HCRyuK/edit?usp=sharing&ouid=106094290608935730038&rtpof=true&sd=true" class="btn btn-outline-info btn-lg " target="_blank"> My
+                        <a href="https://docs.google.com/document/d/1ZZvnRJOo-vOrAwvU0rlj1YT0z-HCRyuK/edit?usp=sharing&ouid=106094290608935730038&rtpof=true&sd=true" className="btn btn-outline-info btn-lg " target="_blank"> My
                             Resume</a>
                         <div className="d-flex flex-row justify-content-center text-info ">
                             <div className="icon">
@@ -55,7 +55,7 @@ function Resume() {
                         <div className="card" role="tablist">
                             <div className="card-header">
 
-                                <div data-toggle="collapse" href="#collapse2" class="">
+                                <div data-toggle="collapse" href="#collapse2" className="">
                                     <h5 className="mb-0">
                                         <i className="fa fa-arrow-circle-down"></i>
                                         Backend Development</h5>
@@ -85,18 +85,18 @@ function Resume() {
                             </div>
 
                         </div>
-                        <div class="card" role="tablist">
-                            <div class="card-header">
-                                <h5 class="mb-0">
+                        <div className="card" role="tablist">
+                            <div className="card-header">
+                                <h5 className="mb-0">
                                     <div data-toggle="collapse" href="#collapse4">
-                                        <i class="fa fa-arrow-circle-down"></i> Graphics Designing
+                                        <i className="fa fa-arrow-circle-down"></i> Graphics Designing
                                     </div>
                                 </h5>
 
                             </div>
-                            <div id="collapse4" class="collapse" data-parent="#accordion">
-                                <div class="card-body">
-                                    <p class="lead">I have Knowledge of photoshop and Illustrator </p>
+                            <div id="collapse4" className="collapse" data-parent="#accordion">
+                                <div className="card-body">
+                                    <p className="lead">I have Knowledge of photoshop and Illustrator </p>
                                 </div>
                             </div>
 
